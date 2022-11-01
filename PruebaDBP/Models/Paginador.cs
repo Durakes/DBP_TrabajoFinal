3b17edb3b50ed902b516917b87acd9ac325028fb
JhenniferD <102687840+JhenniferD@users.noreply.github.com>
2022-10-31 20:15:18 -0500

    Paginacion

---
+++ PruebaDBP/Models/Paginador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace PruebaDBP {
+    public class Paginador<T> where T : class
+    {
+        public int PaginaActual { get; set; }
+        public int RegistrosPorPagina { get; set; }
+        public int TotalRegistros { get; set; }
+        public int TotalPaginas { get; set; }
+        public IEnumerable<T> Resultado { get; set; }
+    }
+}
+
