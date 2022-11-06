fdf6cb7a8bc508e731bd9d5e22d4ae77919096df
jacquiets <93540360+jacquiets@users.noreply.github.com>
2022-11-05 20:20:03 -0500

    Avance login/Registro/Edicion Perfil

--- PruebaDBP/Controllers/UsuarioController.cs
+++ PruebaDBP/Controllers/UsuarioController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-
+using System.Dynamic;
 namespace PruebaDBP.Controllers
 {
     public class UsuarioController : Controller
@@ -21,7 +21,6 @@ namespace PruebaDBP.Controllers
             {
                 var obj = (from userT in Context.Usuarios 
                             where userT.Username == usuario.Username && userT.Contraseña == usuario.Contraseña 
-                        
                             select userT).FirstOrDefault();
                 
                 if(obj != null)
@@ -36,7 +35,7 @@ namespace PruebaDBP.Controllers
             }
             else
             {
-                return RedirectToAction("Login");
+                return View("Login");
             }
             
         }
@@ -54,26 +53,39 @@ namespace PruebaDBP.Controllers
         {
             if(ModelState.IsValid)
             {
-                Usuario nuevoUsuario = new Usuario();
-                nuevoUsuario.NomUsuario = usuario.NomUsuario;
-                nuevoUsuario.ApeUsuario = usuario.ApeUsuario;
-                nuevoUsuario.Username = usuario.Username;
-                nuevoUsuario.Contraseña = usuario.Contraseña;
-                nuevoUsuario.FechaNacimiento = (Convert.ToDateTime(usuario.FechaNacimiento)).ToString("yyyy-MM-dd");
-                var fechaCreacion = DateTime.Now.ToString("yyyy-MM-dd");
-                nuevoUsuario.FechaCreacion = fechaCreacion;
+                var obj = (from userT in Context.Usuarios 
+                            where userT.Username == usuario.Username 
+                            select userT).FirstOrDefault();
                 
-                Context.Usuarios.Add(nuevoUsuario);
-                Context.SaveChanges();
+                if(obj == null)
+                {
+                    Usuario nuevoUsuario = new Usuario();
+                    nuevoUsuario.NomUsuario = usuario.NomUsuario;
+                    nuevoUsuario.ApeUsuario = usuario.ApeUsuario;
+                    nuevoUsuario.Username = usuario.Username;
+                    nuevoUsuario.Contraseña = usuario.Contraseña;
+                    nuevoUsuario.FechaNacimiento = (Convert.ToDateTime(usuario.FechaNacimiento)).ToString("yyyy-MM-dd");
+                    var fechaCreacion = DateTime.Now.ToString("yyyy-MM-dd");
+                    nuevoUsuario.FechaCreacion = fechaCreacion;
+                
+                    Context.Usuarios.Add(nuevoUsuario);
+                    Context.SaveChanges();
 
-                HttpContext.Session.SetString("sUsuario",JsonConvert.SerializeObject(nuevoUsuario));
+                    HttpContext.Session.SetString("sUsuario",JsonConvert.SerializeObject(nuevoUsuario));
                 
 
-                return RedirectToAction("Index");
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    
+                    return View("Registro");
+                }
+
             }
             else
             {
-                return RedirectToAction("Registro");
+                return View("Registro");
             }
 
             
@@ -87,13 +99,109 @@ namespace PruebaDBP.Controllers
         public IActionResult Perfil()
         {
             var usuario = JsonConvert.DeserializeObject<Usuario>(HttpContext.Session.GetString("sUsuario"));
-            Console.WriteLine(usuario.Username);
             return View(usuario);
         }
-
+        [Route("Editar")]
         public IActionResult EditarPerfil()
         {
-            return View();
+            var obj = JsonConvert.DeserializeObject<Usuario>(HttpContext.Session.GetString("sUsuario"));
+            UsuarioModif userMod = new UsuarioModif();
+            userMod.IdUsuario = obj.IdUsuario;
+            userMod.NomUsuario = obj.NomUsuario;
+            userMod.ApeUsuario = obj.ApeUsuario;
+            userMod.Username = obj.Username;
+            userMod.Descripcion = obj.Descripcion;
+            userMod.Contraseña = obj.Contraseña;
+            userMod.UrlFoto = obj.UrlFoto;
+
+            return View(userMod);
         }
+        public IActionResult ModificarPerfil(UsuarioModif obj)
+        {
+            if(ModelState.IsValid)
+            {  
+                var objAnterior = (from userT in Context.Usuarios 
+                                where userT.IdUsuario == obj.IdUsuario
+                                select userT).Single();
+
+                var objUsuario = (from userT in Context.Usuarios 
+                            where userT.Username == obj.Username 
+                            select userT).FirstOrDefault();
+                if(objUsuario == null ) //Si el username no está en la bd
+                {
+                    objAnterior.NomUsuario = obj.NomUsuario;
+                    objAnterior.ApeUsuario = obj.ApeUsuario;
+                    objAnterior.Username = obj.Username;
+                    objAnterior.Descripcion = obj.Descripcion;
+                    objAnterior.UrlFoto = obj.UrlFoto;
+
+                    Context.SaveChanges();
+
+                    HttpContext.Session.Clear();
+                    HttpContext.Session.SetString("sUsuario",JsonConvert.SerializeObject(objAnterior));
+                    return RedirectToAction("Perfil");
+                }
+                else if(objUsuario != null && objUsuario.IdUsuario == obj.IdUsuario) //Si el username está en la bd pero corresponde al mismo usuario
+                {
+                    objAnterior.NomUsuario = obj.NomUsuario;
+                    objAnterior.ApeUsuario = obj.ApeUsuario;
+                    objAnterior.Username = obj.Username;
+                    objAnterior.Descripcion = obj.Descripcion;
+                    objAnterior.UrlFoto = obj.UrlFoto;
+
+                    Context.SaveChanges();
+
+                    HttpContext.Session.Clear();
+                    HttpContext.Session.SetString("sUsuario",JsonConvert.SerializeObject(objAnterior));
+                    return RedirectToAction("Perfil");
+
+                }
+                else //Si el username le corresponde a otro usuario
+                {
+                    return NoContent();
+                }
+                
+            }
+            else
+            {
+                return RedirectToAction("EditarPerfil");
+            }
+            
+        }
+        public IActionResult CambiarContra(UsuarioModif obj)
+        {
+            if(ModelState.IsValid)
+            {  
+                var objAnterior = (from userT in Context.Usuarios 
+                                where userT.IdUsuario == obj.IdUsuario
+                                select userT).Single();
+            
+                if(obj.ContraseñaActual != objAnterior.Contraseña) //Si lo ingresado en contra actual no es igual a la ya existente
+                { 
+                    return NoContent();
+                }
+                else if(obj.ContraseñaNueva1 != obj.ContraseñaNueva2) //Si lo ingresado en contraseña nueva 1 y contraseña nueva 2 no son iguales
+                {
+                    return NoContent();
+                }
+                else
+                {
+                    objAnterior.Contraseña = obj.ContraseñaNueva2;
+                    Context.SaveChanges();
+
+                    HttpContext.Session.Clear();
+                    HttpContext.Session.SetString("sUsuario",JsonConvert.SerializeObject(objAnterior));
+                    return RedirectToAction("Perfil");
+                }
+
+                
+            }
+            else
+            {
+                return RedirectToAction("EditarPerfil");
+            }
+            
+        }
+
     }
 }
